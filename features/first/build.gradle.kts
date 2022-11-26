9cca8b852029a30101a9e8f22ba82dece3e77b38
Victoria <vickgoularte@gmail.com>
2022-11-26 16:09:50 -0300

    ⚙️ Kts to first feature.

---
+++ features/first/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id 'com.android.application'
+    id("kotlin-android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+}
+
+android {compileSdk = BuildConfig.COMPILE_SDK
+    defaultConfig {
+        minSdk = BuildConfig.MIN_SDK
+        targetSdk = BuildConfig.TARGET_SDK
+        testInstrumentationRunner = BuildConfig.TEST_INSTRUMENTATION_RUNNER
+        consumerProguardFiles("proguard-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+}
+
+dependencies {
+    implementation(project(Commons.get()))
+
+    AndroidX.loadAll().forEach { implementation(it) }
+
+    implementation(RetrofitConfig.retrofit)
+
+    Google.loadAll().forEach { implementation(it) }
+    kapt(Google.hiltCompiler)
+
+    JUnit.loadAll().forEach { testImplementation(it) }
+    TestFeature.loadAll().forEach { testImplementation(it) }
+
+    AndroidTest.loadAll().forEach { androidTestImplementation(it) }
+}
